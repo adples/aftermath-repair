5c9d7f99c891c4879b1da15dba828ffca2f52ebc
Alec Plescia <adples.design@gmail.com>
2025-01-17 11:18:23 -0500

    remove center logo

--- functions.php
+++ functions.php
@@ -214,30 +214,6 @@ function acf_should_wrap_innerblocks( $wrap, $name ) {
 	return false;
 }
 
-add_filter('wp_nav_menu_items','add_custom_in_menu', 10, 2);
-
-// add in Logo in the middle of the menu
-//
-function add_custom_in_menu( $items, $args )
-{
-	if( $args->theme_location == 'primary' )
-	{
-		$x = $args->menu->count;
-		if($x % 2 == 0){
-			$insert = $x/2;
-		} else{
-			$insert=floor($x/2)+1;
-		}
-		$new_item       = array( '<li class="navbar-brand-center splice-'.$insert.'">'.get_custom_logo().'</li>' );
-		$items          = preg_replace( '/<\/li>\s<li/', '</li>,<li',  $items );
-
-		$array_items    = explode( ',', $items );
-		array_splice( $array_items, $insert, 0, $new_item ); // splice in at position 3
-		$items          = implode( '', $array_items );
-	}
-	return $items;
-	//return var_dump($args);
-}
 
 /**
  * Gravity Forms - change default button
