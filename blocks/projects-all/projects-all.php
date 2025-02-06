de8d9e660ab84e24e4fddc43e91fd741b93840d5
Alec Plescia <adples.design@gmail.com>
2025-02-06 09:13:38 -0500

    update proj

--- blocks/projects-all/projects-all.php
+++ blocks/projects-all/projects-all.php
@@ -45,7 +45,7 @@ endforeach;
 */
 ?>
 
-<div class="row">
+<div class="row mt-4">
 	<div class="col-lg-3 order-lg-1">
 		<h4 class="h5 text-secondary">Project Search:</h4>
 		<?php echo facetwp_display( 'facet', 'project_name' ); ?>
@@ -61,14 +61,15 @@ endforeach;
 		<button onclick="FWP.reset()" class="btn btn-secondary clear-facets">Clear All Filters&nbsp;&nbsp;<i class="fa fa-times"></i></button>
 	</div>
 	<div class="col-lg-9">
-		<div class="pe-xl-5">
+		<div class="pe-xl-5 facet-results-wrapper">
+			<hr class="hr mt-0 mb-5">
 			<div class="row project-row justify-content-xl-center facetwp-template">
 				
 				<?php while ( $the_query->have_posts() ) : ?>
 					<?php $the_query->the_post(); ?>
 					<?php $id = get_the_ID(); ?>
 					
-						<div id="projectID-<?php echo $id ?>"class="col-md-6 col-xxl-4 project-item">
+						<div id="projectID-<?php echo $id ?>"class="col-md-6 col-xxl-4 project-item xxx">
 							<div class="project-item-inner mb-4">
 								
 								<?php if( get_field('img', $id) ): ?>
@@ -88,6 +89,7 @@ endforeach;
 										<h4 class="project-item-title text-secondary">
 											<?php echo get_field('title', $id); ?>
 										</h4>
+										<span class="btn btn-secondary btn-accent">View Details&nbsp;&nbsp;<i class="fa fa-angle-double-right"></i></span>
 										<?php
 										// $project_type = get_the_terms( $id, 'project-type' );
 										// foreach ($project_type as $type) :
