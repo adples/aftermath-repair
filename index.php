3b2f3915713f615f3ab0aa4e63ed97acfc7be599
Alec Plescia <adples.design@gmail.com>
2025-02-25 14:30:26 -0500

    update blog

--- index.php
+++ index.php
@@ -27,17 +27,34 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 		
-		<div class="banner banner-interior">
+		<?php 
+		if( get_field('blog_banner','option') ): 
+			if( get_field('bg','option') ):
+				$banner_img = get_field('bg','option');
+				$banner_img_md = wp_get_attachment_image_src($banner_img['id'], 'medium_large');
+				$banner_img_full = wp_get_attachment_image_src($banner_img['id'], 'full');
 				
-				<div class="banner-img resize">
+				$attributes = 'class="banner-img resize"';
+				$attributes .= 'style="background-image: url('.$banner_img_md[0].')" ';
+				$attributes .= 'data-img-md="'.$banner_img_md[0].'" ';
+				$attributes .= 'data-img-full="'.$banner_img_full[0].'"';
+			?>
+				<div class="banner banner-interior">
+					<div class="container">
+						<div <?php echo $attributes ?> >
+						</div>
+						
+						<?php 
+						if (get_field('breadcrumb','option')){
+							if ( function_exists('yoast_breadcrumb') ) {
+								yoast_breadcrumb( '<div class="breadcrumb-wrapper"><div id="breadcrumbs" class="breadcrumbs">','</div></div>' );
+							}
+						}
+						?>
+					</div>
 				</div>
-				
-				<?php 
-				if ( function_exists('yoast_breadcrumb') ) {
-					yoast_breadcrumb( '<div class="breadcrumb-wrapper"><div id="breadcrumbs" class="breadcrumbs">','</div></div>' );
-				}
-				?>
-		</div>
+			<?php endif; ?>
+		<?php endif; ?>
 		
 		<div>
 
@@ -48,12 +65,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<main class="site-main row gx-xxl-5" id="main">
 				
-				<h1 class="accent-title h1 text-center text-secondary"><span>Foundation Repair Blog</span></h1>
-				<div class="row justify-content-center mb-4 mb-x-5">
-					<div class="col-lg-9">
-						<p class="font-up text-center">Welcome to the Aftermath Structural Repair blog. We specialize in strengthening the foundation of your home or business with long-lasting solutions that ensure structural stability and your peace of mind. Explore our tips, industry insights, and success stories to keep your property safe and sound.</p>
+				<?php if( get_field('blog_title','option') ): ?>
+					<h1 class="accent-title h1 text-center text-secondary"><span><?php echo get_field('blog_title','option') ?></span></h1>
+				<?php endif; ?>
+				
+				<?php if( get_field('blog_lead','option') ): ?>
+					<div class="row justify-content-center mb-4 mb-x-5">
+						<div class="col-lg-9">
+							<p class="font-up text-center"><?php echo get_field('blog_lead','option') ?></p>
+						</div>
 					</div>
-				</div>
+				<?php endif; ?>
 
 				<?php
 				if ( have_posts() ) {
